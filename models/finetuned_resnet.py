1aee39bb7691829bddbc89b8135fa132cf7763f3
han-keong <hk997@live.com>
2021-04-30 09:49:50 +0800

    finetuned resnet

---
+++ models/finetuned_resnet.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import os
+
+def finetuned_resnet(num_classes, weights_dir=None, include_top=False):
+    # Load pretrained ResNet50 model
+    model = models.resnet50(pretrained=True)
+    
+    # Freeze model parameters
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    # Replace final fully-connected layer
+    fc_layers = [
+        nn.Linear(model.fc.in_features, 2048),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+        nn.Linear(2048, 1024),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+    ]
+    
+    if include_top:
+        fc_layers.append(nn.Linear(1024, num_classes))
+        fc_layers.append(nn.Softmax(dim=1))
+    
+    model.fc = nn.Sequential(*fc_layers)
+    
+    # Load weights
+    if weights_dir is not None and os.path.exists(weights_dir):
+        model.load_state_dict(torch.load(weights_dir))
+    
+    return model
