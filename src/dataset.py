5b10882bd280e98d898ffb557b8372832b9f569b
han-keong <hk997@live.com>
2021-05-02 17:00:21 +0000

    update source code

--- src/dataset.py
+++ src/dataset.py
@@ -1,62 +1,94 @@
 import os
+import torch
+import numpy as np
 import pandas as pd
 
-from utils import read_clip
+from .utils import read_clip
 from torch.utils.data import Dataset
 
-class SSV2_Dataset(Dataset):
-    """Something Something V2 Dataset"""
+class CustomDataset(Dataset):
+    """Custom Dataset"""
     
-    def __init__(self, root, dstype, transform=None):
+    def __init__(self, root, subdir, dstype, ext, transform=None):
         """
         Args:
-            root (string): Root directory (see directory structure)
-            dstype (string): Type of dataset ('train' or 'test')
-            transform (callable, optional): Optional transform to be applied on a sample.
+            root (string): Root directory
+            subdir (string): Root sub-directory
+            dstype (string): Dataset type ('train' or 'test')
+            ext (string): Data files file extension
+            transform (callable, optional): Optional transform to be applied on a sample
         
         Directory Structure:
-            root/
-            ├── raw/
-            │   ├── train/
-            │   └── test/
-            │       ├── 123/          # Class index
-            │       │   ├── 456.webm  # Raw clip files
-            │       │   └── ..
-            │       └── ..
-            ├── train_list.csv        # Clip id, label (class index)
-            ├── test_list.csv         # Clip id, label (class index)
+            root/                     # Root directory
+            ├── subdir/               # Sub-directory
+            │   └── dstype/           # Dataset type
+            │       └── 123/          # Class index
+            │           └── 456.ext   # Data files
+            ├── train_list.csv        # Sample id, class index
+            ├── test_list.csv         # Sample id, class index
             └── classes.txt           # List of class names
         """
         assert dstype in ('train', 'test'), f"dstype must be 'train' or 'test', not '{dstype}'"
         
         self.root = root
+        self.subdir = subdir
         self.dstype = dstype
+        self.ext = ext
         self.transform = transform
         
         self._load_classes()
-        self._load_clip_list()
+        self._load_sample_list()
     
     def _load_classes(self):
         with open(os.path.join(self.root, 'classes.txt'), 'r') as f:
-            self.classes = [line.strip() for line in f]
-            self.num_classes = len(self.classes)
+            self._classes = [line.strip() for line in f]
+            self._num_classes = len(self._classes)
     
-    def _load_clip_list(self):
-        self.clip_list = pd.read_csv(f'{self.dstype}_list.csv')
+    def _load_sample_list(self):
+        list_path = os.path.join(self.root, f'{self.dstype}_list.csv')
+        self._sample_list = pd.read_csv(list_path)
+        self._num_samples = len(self._sample_list)
+    
+    def get_info(self, idx):
+        return tuple(self._sample_list.iloc[idx])
+    
+    def _get_sample(self, path):
+        raise NotImplementedError
+    
+    def _get_label(self, cls_idx):
+        label = torch.zeros(self._num_samples)
+        label[cls_idx] = 1.
+        return label
     
     def __len__(self):
-        return len(self.clip_paths)
+        return self._num_samples
     
     def __getitem__(self, idx):
-        clip_id, cls_idx = self.clip_list.iloc[idx]
+        sample_id, cls_idx = self.get_info(idx)
+        name = str(sample_id) + os.extsep + self.ext
+        path = os.path.join(self.root, self.subdir, self.dstype, str(cls_idx), name)
         
-        label = torch.zeros(self.num_classes)
-        label[cls_idx] = 1.
-        
-        clip_path = os.path.join(self.root, self.dstype, cls_idx, clip_id)
-        clip = read_clip(clip_path)
-        
-        if self.transform:
+        sample = self._get_sample(path)
+        label = self._get_label(cls_idx)
+        return sample, label
+
+class SSV2_Dataset(CustomDataset):
+    """Something Something V2 Dataset"""
+
+    def __init__(self, root, subdir, dstype, transform=None):
+        super().__init__(root, subdir, dstype, 'webm', transform)
+    
+    def _get_sample(self, path):
+        clip = read_clip(path)
+        if self.transform is not None:
             clip = self.transform(clip)
-        
-        return clip, label
+        return clip
+
+class SSV2_ProcessedDataset(CustomDataset):
+    """Something Something V2 Processed Dataset"""
+    
+    def __init__(self, root, subdir, dstype):
+        super.__init__(root, subdir, dstype, 'npy')
+    
+    def _get_sample(self, path):
+        return torch.from_numpy(np.load(path))
--- src/functional.py
+++ src/functional.py
@@ -1,33 +1,65 @@
-import random
-import torch
 import cv2
+import torch
+import random
+import numpy as np
 
-def select_random_frame(clip):
-    clip_len = clip.shape[0] # L x H x W x C
+from torchvision.transforms.functional import (
+    to_tensor, to_pil_image, normalize, resize, scale,
+    pad, crop, center_crop, resized_crop, hflip, vflip,
+    perspective, five_crop, ten_crop, adjust_brightness,
+    adjust_contrast, adjust_saturation, adjust_hue,
+    adjust_gamma, rotate, affine, to_grayscale, erase,
+)
+
+from torchvision.transforms._functional_video import (
+    crop as crop_video,
+    resize as resize_video,
+    resized_crop as resized_crop_video,
+    center_crop as center_crop_video,
+    normalize as normalize_video,
+    hflip as hflip_video,
+)
+
+def select_random_frame(clip): # L x H x W x C -> H x W x C
+    clip_len = clip.shape[0]
     i = random.randrange(clip_len)
-    return clip[i] # H x W x C
+    return clip[i]
 
-def select_random_frames(clip, num_frames):
-    clip_len = clip.shape[0] # L x H x W x C
-    step = int(clip_len / (num_frames+1))
+def select_random_frames(clip, num_frames): # L x H x W x C -> # OL x H x W x C
+    clip_len = clip.shape[0]
+    if clip_len < num_frames:
+        raise ValueError(f'The given clip has insufficient frames ({clip_len}/{num_frames})')
+    elif clip_len == num_frames:
+        return clip
+    step = clip_len // (num_frames+1) 
     frames = []
     for i in range(num_frames):
         i = i*step + random.randrange(step)
         frames.append(clip[i])
-    return torch.stack(frames, dim=0) # OL x H x W x C
+    return np.stack(frames, axis=0)
 
-def random_trim_video(clip, num_frames):
-    clip_len = clip.shape[0] # L x H x W x C
-    start = random.randrange(clip_len - num_frames)
+def select_random_trim(clip, num_frames): # L x H x W x C -> # OL x H x W x C
+    if clip_len < num_frames:
+        raise ValueError(f'The given clip has insufficient frames ({clip_len}/{num_frames})')
+    elif clip_len == num_frames:
+        return clip
+    start = random.randrange(clip.shape[0] - num_frames)
     end = start + num_frames
-    return clip[start:end] # OL x H x W x C
+    return clip[start:end]
 
-def to_optical_flow(clip):
-    clip_len = clip.shape[0] # L x H x W x C
+def to_tensor_video(clip): # L x H x W x C -> C x L x H x W
+    return torch.from_numpy(clip).float().permute(3, 0, 1, 2).divide(255.0)
+
+def to_numpy_video(tensor): # C x L x H x W -> L x H x W x C
+    return tensor.mul(255.0).permute(1, 2, 3, 0).byte().numpy()
+
+def to_optical_flow(clip): # L x H x W x C -> OL x H x W x 2, where OL = L-1
+    # See https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
+    clip_len = clip.shape[0]
     flows = []
     for i in range(clip_len-1):
-        before = cv2.cvtColor(clip[i], cv2.COLOR_BGR2GRAY)
-        after = cv2.cvtColor(clip[i+1], cv2.COLOR_BGR2GRAY)
+        before = cv2.cvtColor(clip[i], cv2.COLOR_RGB2GRAY)
+        after = cv2.cvtColor(clip[i+1], cv2.COLOR_RGB2GRAY)
         flow = cv2.calcOpticalFlowFarneback(before, after, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        flows.append(torch.from_numpy(flow)) # See https://docs.opencv.org/3.4/d4/dee/tutorial_optical_flow.html
-    return torch.stack(flows, dim=0) # OL x H x W x 2, where OL = 2*(L-1)
+        flows.append(flow)
+    return np.stack(flows, axis=0)
